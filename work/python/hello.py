d2f130551dcb552855136a13bf0b3dd768268aa8
Steven <stevenhughes08@gmail.com>
2019-11-02 18:58:01 -0400

    hello.py is decision making python app, simple but for learning

---
+++ work/python/hello.py
@@ -0,0 +1,5 @@
+first_name = input("What is your first name? ")
+print("Hello,", first_name)
+if first_name == "Steve":
+    print(first_name, "is Learning Python")
+    print("have a great day {}:".format(first_name))
